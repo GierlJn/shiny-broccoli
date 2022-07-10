950810c983ea659cde2592cfa0e91a3debbeda17
Julian <julian.gierl@gmx.de>
2022-07-10 14:22:00 +0200

    Initial Commit

---
+++ YourHabs/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  YourHabs
+//
+//  Created by Julian Gierl on 10.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YourHabs/YourHabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourHabsApp.swift
+//  YourHabs
+//
+//  Created by Julian Gierl on 10.07.22.
+//
+
+import SwiftUI
+
+@main
+struct YourHabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
